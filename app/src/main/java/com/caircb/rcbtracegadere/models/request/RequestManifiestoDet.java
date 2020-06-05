4ee8a5aa43f534c6516aa26acfdb4e223223bd03
Anabely <afernandez@nimbussoft.net>
2020-06-05 01:48:47 -0500

    paquetes

--- app/src/main/java/com/caircb/rcbtracegadere/models/request/RequestManifiestoDet.java
+++ app/src/main/java/com/caircb/rcbtracegadere/models/request/RequestManifiestoDet.java
@@ -9,4 +9,35 @@ public class RequestManifiestoDet {
     private BigDecimal cantidad;
     private List<RequestManifiestoDetBultos> bultos;
 
+    public Integer getIdAppManifiestoDetalle() {
+        return idAppManifiestoDetalle;
+    }
+
+    public void setIdAppManifiestoDetalle(Integer idAppManifiestoDetalle) {
+        this.idAppManifiestoDetalle = idAppManifiestoDetalle;
+    }
+
+    public BigDecimal getPeso() {
+        return peso;
+    }
+
+    public void setPeso(BigDecimal peso) {
+        this.peso = peso;
+    }
+
+    public BigDecimal getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(BigDecimal cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public List<RequestManifiestoDetBultos> getBultos() {
+        return bultos;
+    }
+
+    public void setBultos(List<RequestManifiestoDetBultos> bultos) {
+        this.bultos = bultos;
+    }
 }
--- app/src/main/java/com/caircb/rcbtracegadere/models/request/RequestManifiestoNovedadFrecuente.java
+++ app/src/main/java/com/caircb/rcbtracegadere/models/request/RequestManifiestoNovedadFrecuente.java
@@ -5,4 +5,20 @@ import java.util.List;
 public class RequestManifiestoNovedadFrecuente {
     private Integer idCatalogo;
     private List<RequestNovedadFoto> fotos;
+
+    public Integer getIdCatalogo() {
+        return idCatalogo;
+    }
+
+    public void setIdCatalogo(Integer idCatalogo) {
+        this.idCatalogo = idCatalogo;
+    }
+
+    public List<RequestNovedadFoto> getFotos() {
+        return fotos;
+    }
+
+    public void setFotos(List<RequestNovedadFoto> fotos) {
+        this.fotos = fotos;
+    }
 }
