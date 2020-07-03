4f93c1bc0ca26caf524db040187d30b3ec8d18f5
fcatagna <vcatagna@raulcoka.com>
2020-07-03 11:37:38 -0500

    Fragment ruta asignada filter
    Signed-off-by: fcatagna <vcatagna@raulcoka.com>

---
+++ app/src/main/java/com/caircb/rcbtracegadere/adapters/ManifiestoDetalleAdapterSede.java
@@ -0,0 +1,123 @@
+package com.caircb.rcbtracegadere.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.caircb.rcbtracegadere.MyApp;
+import com.caircb.rcbtracegadere.R;
+import com.caircb.rcbtracegadere.database.AppDatabase;
+import com.caircb.rcbtracegadere.models.RowItemManifiesto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManifiestoDetalleAdapterSede extends RecyclerView.Adapter<ManifiestoDetalleAdapterSede.MyViewHolder>  {
+
+    private ClickListener mClickListener;
+    private Context mContext;
+    private List<RowItemManifiesto> manifiestosDtList;
+    private String numeroManifiesto;
+    private Integer estadoManifiesto;
+
+    public ManifiestoDetalleAdapterSede(Context context, String numeroManifiesto, Integer estadoManifiesto){
+        this.mContext = context;
+        this.manifiestosDtList = new ArrayList<>();
+        this.numeroManifiesto=numeroManifiesto;
+        this.estadoManifiesto =estadoManifiesto;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.lista_items_manifiesto_sede,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final @NonNull MyViewHolder holder, int position) {
+        final RowItemManifiesto it = manifiestosDtList.get(position);
+        holder.txtUnidad.setText(it.getUnidad());
+        holder.txtPeso.setText(""+it.getPeso());
+        holder.txtCantidadBulto.setText(""+it.getCantidadBulto());
+        holder.txtDescripcion.setText(it.getDescripcion());
+        holder.txtTratamiento.setText(it.getTratamiento());
+        holder.chkEstado.setChecked(it.isEstado());
+
+        if(estadoManifiesto !=1) {
+            holder.chkEstado.setClickable(false);
+        }
+        if(estadoManifiesto ==1) {
+            holder.chkEstado.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (((CheckBox) v).isChecked()) {
+                        v.setSelected(true);
+                        it.setEstado(true);
+                    } else {
+                        v.setSelected(false);
+                        it.setEstado(false);
+                    }
+                    MyApp.getDBO().manifiestoDetalleDao().updateManifiestoDetallebyId(it.getId(), it.isEstado(), it.isEstado() ? AppDatabase.getUUID(numeroManifiesto) : "");
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return manifiestosDtList.size();
+    }
+
+    public void setTaskList(List<RowItemManifiesto> taskList) {
+        this.manifiestosDtList = taskList;
+        notifyDataSetChanged();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtUnidad;
+        TextView txtPeso;
+        TextView txtCantidadBulto;
+        TextView txtDescripcion;
+        TextView txtTratamiento;
+        LinearLayout btnEleminarItem;
+        CheckBox chkEstado;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            txtUnidad = itemView.findViewById(R.id.txtItemUnidad);
+            txtPeso = itemView.findViewById(R.id.txtItemPeso);
+            txtCantidadBulto = itemView.findViewById(R.id.txtItemCantidadBulto);
+            txtDescripcion = itemView.findViewById(R.id.txtItemDescripcion);
+            txtTratamiento = itemView.findViewById(R.id.txtItemTratamiento);
+            btnEleminarItem = itemView.findViewById(R.id.btnEleminarItem);
+            chkEstado = itemView.findViewById(R.id.chkEstadoItemDetalle);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mClickListener!=null)mClickListener.onItemClick(getAdapterPosition(),v);
+                }
+            });
+
+        }
+
+    }
+
+    public void setOnItemClickListener(@NonNull ClickListener l) {
+        mClickListener= l;
+    }
+
+    public interface ClickListener {
+        void onItemClick(int position, View v);
+    }
+}
--- app/src/main/java/com/caircb/rcbtracegadere/fragments/Sede/ManifiestoSedeFragment.java
+++ app/src/main/java/com/caircb/rcbtracegadere/fragments/Sede/ManifiestoSedeFragment.java
@@ -8,21 +8,45 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TabHost;
 
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.caircb.rcbtracegadere.MyApp;
 import com.caircb.rcbtracegadere.R;
+import com.caircb.rcbtracegadere.adapters.ManifiestoDetalleAdapter;
+import com.caircb.rcbtracegadere.adapters.ManifiestoDetalleAdapterSede;
 import com.caircb.rcbtracegadere.fragments.GestorAlterno.HojaRutaAsignadaGestorFragment;
 import com.caircb.rcbtracegadere.fragments.GestorAlterno.RecepcionGestorFragment;
+import com.caircb.rcbtracegadere.fragments.recolector.manifiesto2.Manifiesto2Fragment;
 import com.caircb.rcbtracegadere.generics.MyFragment;
 import com.caircb.rcbtracegadere.generics.OnCameraListener;
+import com.caircb.rcbtracegadere.models.RowItemManifiesto;
 import com.caircb.rcbtracegadere.tasks.UserRegistrarPlanta;
+
+import java.util.List;
 
 
 public class ManifiestoSedeFragment extends MyFragment implements OnCameraListener, View.OnClickListener {
-    LinearLayout btnManifiestoCancel, btnManifiestoNext;
+    LinearLayout btnManifiestoCancel;
+    private RecyclerView recyclerView;
     private static final String ARG_PARAM1 = "manifiestoID";
     RecepcionGestorFragment manifiestoGestor;
-    Integer idAppManifiesto;
+    Integer idAppManifiesto,estadoManifiesto;
     UserRegistrarPlanta userRegistrarPlanta;
+    ManifiestoDetalleAdapterSede recyclerviewAdapter;
+    private List<RowItemManifiesto> detalles;
 
+    public  ManifiestoSedeFragment (){
+    }
+
+    public static ManifiestoSedeFragment newInstance(Integer manifiestoID) {
+        ManifiestoSedeFragment f= new ManifiestoSedeFragment();
+        Bundle b = new Bundle();
+        b.putInt(ARG_PARAM1,manifiestoID);
+        f.setArguments(b);
+        return f;
+    }
 
     @Override
     public void onClick(View v) {
@@ -56,14 +80,37 @@ public class ManifiestoSedeFragment extends MyFragment implements OnCameraListen
         // Inflate the layout for this fragment
         setView(inflater.inflate(R.layout.fragment_hoja_ruta_sede, container, false));
         init();
+        loadData();
         return getView();
     }
 
     public void init(){
         btnManifiestoCancel = getView().findViewById(R.id.btnRetornarDetalleSede);
         btnManifiestoCancel.setOnClickListener(this);
+        recyclerView = getView().findViewById(R.id.recyclerview);
+        recyclerviewAdapter = new ManifiestoDetalleAdapterSede(getActivity(),idAppManifiesto.toString(),1);
         manifiestoGestor = new RecepcionGestorFragment(getActivity(),idAppManifiesto);
     }
+
+    private void loadData(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+        detalles = MyApp.getDBO().manifiestoDetalleDao().fetchHojaRutaDetallebyIdManifiesto(idAppManifiesto);
+
+        recyclerviewAdapter.setTaskList(detalles);
+        recyclerView.setAdapter(recyclerviewAdapter);
+
+        recyclerviewAdapter.setOnItemClickListener(new ManifiestoDetalleAdapterSede.ClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                int x=0;
+                if(estadoManifiesto == 1){
+                    //openOpcionesItems(position);
+                }
+            }
+        });
+    }
+
 
 
 
@@ -71,11 +118,4 @@ public class ManifiestoSedeFragment extends MyFragment implements OnCameraListen
         return new ManifiestoSedeFragment();
     }
 
-    public static ManifiestoSedeFragment newInstance(Integer manifiestoID) {
-        ManifiestoSedeFragment f= new ManifiestoSedeFragment();
-        Bundle b = new Bundle();
-        b.putInt(ARG_PARAM1,manifiestoID);
-        f.setArguments(b);
-        return f;
-    }
 }
