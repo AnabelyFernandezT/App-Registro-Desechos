0635406c94ca748072e2b8795e2ae8c7c0977bcf
Jose Luis Suquillo Topon <jsuquillo@rcoka.int>
2020-07-04 17:49:45 -0500

    Impresión de etiquetas QR, no funcionaba el hilo

--- app/src/main/java/com/caircb/rcbtracegadere/database/dao/ManifiestoDetallePesosDao.java
+++ app/src/main/java/com/caircb/rcbtracegadere/database/dao/ManifiestoDetallePesosDao.java
@@ -52,12 +52,13 @@ public abstract class ManifiestoDetallePesosDao {
             " inner join tb_catalogos c on dt.idTipoDesecho = c.idSistema and c.tipo=2 order by m.idAppManifiesto,dt.idAppManifiestoDetalle,b._id")
     public  abstract List<ItemEtiqueta> consultarBultosImpresion(Integer idAppManifiesto);
 
-    @Query("select dt.idAppManifiestoDetalle,m.nombreCliente as cliente,m.numeroManifiesto,m.fechaRecoleccion," +
+    @Query("select dt.idAppManifiestoDetalle,m.nombreCliente as cliente,m.numeroManifiesto,rif.fechaInicio as fechaRecoleccion," +
             "c.nombre as residuo,dt.tratamiento,valor as peso, b.codeQr as codigoQr,0 as indexEtiqueta, dt.cantidadTotalEtiqueta as totalEtiqueta " +
             " from tb_manifiesto_detalle_pesos b" +
             " inner join tb_manifiestos_detalle dt on b.idAppManifiestoDetalle=dt.idAppManifiestoDetalle  " +
             " inner join tb_manifiestos m on dt.idAppManifiesto=m.idAppManifiesto " +
             " inner join tb_catalogos c on dt.idTipoDesecho = c.idSistema and c.tipo=2 " +
+            "inner join tb_rutaInicioFin rif on rif.idSubRuta = m.idSubRuta " +
             " where m.idAppManifiesto=:idAppManifiesto and dt.idAppManifiestoDetalle =:idManifiestoDetalle and b._id =:idCatalogo " +
             " order by m.idAppManifiesto,dt.idAppManifiestoDetalle,b._id")
     public  abstract ItemEtiqueta consultaBultoIndividual(Integer idAppManifiesto, Integer idManifiestoDetalle, Integer idCatalogo);
