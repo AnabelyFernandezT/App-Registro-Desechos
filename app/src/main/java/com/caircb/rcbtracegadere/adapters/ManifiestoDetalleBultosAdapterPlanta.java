1338358a432cfe6fca49914ae120f54dd51c089e
fcatagna <vcatagna@raulcoka.com>
2020-07-11 16:15:51 -0500

    Fragment ruta asignada filter
    Signed-off-by: fcatagna <vcatagna@raulcoka.com>

--- app/src/main/java/com/caircb/rcbtracegadere/adapters/ManifiestoDetalleBultosAdapterPlanta.java
+++ app/src/main/java/com/caircb/rcbtracegadere/adapters/ManifiestoDetalleBultosAdapterPlanta.java
@@ -1,10 +1,13 @@
 package com.caircb.rcbtracegadere.adapters;
 
 import android.content.Context;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -44,21 +47,39 @@ public class ManifiestoDetalleBultosAdapterPlanta extends RecyclerView.Adapter<M
         holder.txtPeso.setText(it.getPeso().toString());
         holder.txtNombre.setText(it.getNombreBulto());
         holder.chkEstado.setChecked(it.getEstado());
-            holder.chkEstado.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (((CheckBox) v).isChecked()) {
-                        v.setSelected(true);
-                        it.setEstado(true);
-                    } else {
-                        v.setSelected(false);
-                        it.setEstado(false);
-                    }
-                    MyApp.getDBO().manifiestoPlantaDetalleValorDao().updateManifiestoDetalleValorSedebyId(it.getIdManifiestoDetalle(), it.getEstado(),it.getIdManifiestoDetalleValores());
-                }
-            });
+        holder.txtNuevoPeso.setText(it.getNuevoPeso()==null ? "":it.getNuevoPeso().toString());
 
-    }
+        holder.txtNuevoPeso.setFocusable(true);
+        holder.txtNuevoPeso.setFocusableInTouchMode(true);
+        holder.txtNuevoPeso.requestFocus();
+
+          holder.chkEstado.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  String nuevoValor = holder.txtNuevoPeso.getText().toString() == "" ? "0": holder.txtNuevoPeso.getText().toString();
+                  Integer valida = 0;
+                  if(nuevoValor.equals("")){
+                      valida = 0;
+                      holder.chkEstado.setChecked(false);
+                  }else{
+                      valida=1;
+                  }
+
+                  if(valida.equals(1) ) {
+                  if (((CheckBox) v).isChecked()) {
+                      v.setSelected(true);
+                      it.setEstado(true);
+                  } else {
+                      v.setSelected(false);
+                      it.setEstado(false);
+                  }
+                  MyApp.getDBO().manifiestoPlantaDetalleValorDao().updateManifiestoDetalleValorPlantaPesoNuevo(it.getIdManifiestoDetalle(), (holder.txtNuevoPeso.getText().toString()), it.getIdManifiestoDetalleValores());
+                  MyApp.getDBO().manifiestoPlantaDetalleValorDao().updateManifiestoDetalleValorSedebyId(it.getIdManifiestoDetalle(), it.getEstado(), it.getIdManifiestoDetalleValores());
+              }
+              }
+          });
+      }
+
 
     @Override
     public int getItemCount() {
@@ -75,13 +96,32 @@ public class ManifiestoDetalleBultosAdapterPlanta extends RecyclerView.Adapter<M
         TextView txtNombre;
         TextView txtPeso;
         CheckBox chkEstado;
-
+        EditText txtNuevoPeso;
         public MyViewHolder(View itemView) {
             super(itemView);
 
             txtNombre = itemView.findViewById(R.id.txtNombre);
             txtPeso = itemView.findViewById(R.id.txtBultos);
             chkEstado = itemView.findViewById(R.id.chkEstadoItemDetalle);
+            txtNuevoPeso = (EditText)itemView.findViewById(R.id.txtIgnBultos);
+            txtNuevoPeso.requestFocus();
+            txtNuevoPeso.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if(hasFocus){
+
+                    }
+                }
+            });
+
+            itemView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if(hasFocus){
+
+                    }
+                }
+            });
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -93,6 +133,7 @@ public class ManifiestoDetalleBultosAdapterPlanta extends RecyclerView.Adapter<M
         }
 
     }
+
 
     public void setOnItemClickListener(@NonNull ClickListener l) {
         mClickListener= l;
