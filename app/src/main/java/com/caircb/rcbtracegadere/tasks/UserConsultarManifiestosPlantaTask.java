c126e3a50a3ced2f2b0f98a28a2d991e82e13b87
fcatagna <vcatagna@raulcoka.com>
2020-07-13 16:03:32 -0500

    Fragment ruta asignada filter
    Signed-off-by: fcatagna <vcatagna@raulcoka.com>

--- app/src/main/java/com/caircb/rcbtracegadere/tasks/UserConsultarManifiestosPlantaTask.java
+++ app/src/main/java/com/caircb/rcbtracegadere/tasks/UserConsultarManifiestosPlantaTask.java
@@ -41,9 +41,9 @@ public class UserConsultarManifiestosPlantaTask extends MyRetrofitApi implements
             public void onResponse(Call<List<DtoManifiestoPlanta>> call, Response<List<DtoManifiestoPlanta>> response) {
                 if (response.isSuccessful()){
                     if(mOnVehiculoListener!=null)mOnVehiculoListener.onSuccessful(response.body());
-                    MyApp.getDBO().manifiestoPlantaDao().eliminarManifiestos();
-                    MyApp.getDBO().manifiestoPlantaDetalleDao().eliminarDetalle();
-                    MyApp.getDBO().manifiestoPlantaDetalleValorDao().eliminarDetalle();
+                    //MyApp.getDBO().manifiestoPlantaDao().eliminarManifiestos();
+                    //MyApp.getDBO().manifiestoPlantaDetalleDao().eliminarDetalle();
+                    //MyApp.getDBO().manifiestoPlantaDetalleValorDao().eliminarDetalle();
 
                     for(DtoManifiestoPlanta reg:response.body()){
                         MyApp.getDBO().manifiestoPlantaDao().saveOrUpdate(reg);
