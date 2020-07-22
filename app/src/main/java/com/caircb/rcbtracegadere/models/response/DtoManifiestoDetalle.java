ea3922e23c18762b978006c24cbd72bcb084f16d
cproano <cproanio@nimbussoft.net>
2020-07-21 20:05:20 -0500

    Agregado campos nuevos consulta DtoManifiesto

--- app/src/main/java/com/caircb/rcbtracegadere/models/response/DtoManifiestoDetalle.java
+++ app/src/main/java/com/caircb/rcbtracegadere/models/response/DtoManifiestoDetalle.java
@@ -21,6 +21,12 @@ public class DtoManifiestoDetalle {
     private String validadorReferencial;
     private String tipoContenedor;
     private String estadoFisico;
+    private Integer residuoSujetoFiscalizacion;
+    private Integer requiereDevolucionRecipientes;
+    private Integer tieneDisponibilidadMontacarga;
+    private Integer tieneDisponibilidadBalanza;
+    private Integer requiereIncineracionPresenciada;
+    private String observacionResiduos;
 
     public DtoManifiestoDetalle() {
     }
@@ -167,5 +173,53 @@ public class DtoManifiestoDetalle {
 
     public void setEstadoFisico(String estadoFisico) {
         this.estadoFisico = estadoFisico;
+    }
+
+    public Integer getResiduoSujetoFiscalizacion() {
+        return residuoSujetoFiscalizacion;
+    }
+
+    public void setResiduoSujetoFiscalizacion(Integer residuoSujetoFiscalizacion) {
+        this.residuoSujetoFiscalizacion = residuoSujetoFiscalizacion;
+    }
+
+    public Integer getRequiereDevolucionRecipientes() {
+        return requiereDevolucionRecipientes;
+    }
+
+    public void setRequiereDevolucionRecipientes(Integer requiereDevolucionRecipientes) {
+        this.requiereDevolucionRecipientes = requiereDevolucionRecipientes;
+    }
+
+    public Integer getTieneDisponibilidadMontacarga() {
+        return tieneDisponibilidadMontacarga;
+    }
+
+    public void setTieneDisponibilidadMontacarga(Integer tieneDisponibilidadMontacarga) {
+        this.tieneDisponibilidadMontacarga = tieneDisponibilidadMontacarga;
+    }
+
+    public Integer getTieneDisponibilidadBalanza() {
+        return tieneDisponibilidadBalanza;
+    }
+
+    public void setTieneDisponibilidadBalanza(Integer tieneDisponibilidadBalanza) {
+        this.tieneDisponibilidadBalanza = tieneDisponibilidadBalanza;
+    }
+
+    public Integer getRequiereIncineracionPresenciada() {
+        return requiereIncineracionPresenciada;
+    }
+
+    public void setRequiereIncineracionPresenciada(Integer requiereIncineracionPresenciada) {
+        this.requiereIncineracionPresenciada = requiereIncineracionPresenciada;
+    }
+
+    public String getObservacionResiduos() {
+        return observacionResiduos;
+    }
+
+    public void setObservacionResiduos(String observacionResiduos) {
+        this.observacionResiduos = observacionResiduos;
     }
 }
