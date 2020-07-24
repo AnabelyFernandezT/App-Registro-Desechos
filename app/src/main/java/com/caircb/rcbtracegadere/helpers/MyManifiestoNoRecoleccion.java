698eedc9441a2d56753b62042bb8fc19c20ca022
Anabely <afernandez@nimbussoft.net>
2020-07-24 01:40:21 -0500

    bug vista previa manifiestos

--- app/src/main/java/com/caircb/rcbtracegadere/helpers/MyManifiestoNoRecoleccion.java
+++ app/src/main/java/com/caircb/rcbtracegadere/helpers/MyManifiestoNoRecoleccion.java
@@ -597,9 +597,19 @@ public class MyManifiestoNoRecoleccion {
         tb6.addCell(_cell);
 
         Paragraph para2 = new Paragraph();
-        para2.add(new Chunk(manifiesto.getConductorNombre(),f6));
+        if(manifiesto.getConductorNombre()!= null){
+            para2.add(new Chunk(manifiesto.getConductorNombre(),f6));
+        }else{
+            para2.add(new Chunk("",f6));
+        }
+
         para2.add(Chunk.NEWLINE);
-        para2.add(new Chunk(manifiesto.getConductorIdentificacion(),f6));
+        if(manifiesto.getConductorIdentificacion()!=null){
+            para2.add(new Chunk(manifiesto.getConductorIdentificacion(),f6));
+        }else{
+            para2.add(new Chunk("",f6));
+        }
+
         para2.add(Chunk.NEWLINE);
         _cell = new PdfPCell(para2);
         _cell.setBorder(Rectangle.NO_BORDER);
