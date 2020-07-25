c98b56f175fb7c8e43e9c8e024b6d6eccdb387b6
marosero <marosero@raulcoka.com>
2020-07-25 00:33:57 -0500

    calculadoraBultosParaPlanta

--- app/src/main/java/com/caircb/rcbtracegadere/dialogs/DialogBultosPlanta.java
+++ app/src/main/java/com/caircb/rcbtracegadere/dialogs/DialogBultosPlanta.java
@@ -71,7 +71,7 @@ public class DialogBultosPlanta extends MyDialog {
                 boolean bandera = false;
                 List<ItemManifiestoDetalleValorSede> lista = MyApp.getDBO().manifiestoPlantaDetalleValorDao().fetchManifiestosAsigByClienteOrNumManif(idAppManifiestoDet);
                 for(ItemManifiestoDetalleValorSede reg: lista){
-                    if(reg.getNuevoPeso() != null && !(reg.getEstado())) {
+                    if(reg.getNuevoPeso() > 0 && !(reg.getEstado())) {
                         messageBox("Debe seleccionar todos los bultos");
                         bandera = true;
                         return;
