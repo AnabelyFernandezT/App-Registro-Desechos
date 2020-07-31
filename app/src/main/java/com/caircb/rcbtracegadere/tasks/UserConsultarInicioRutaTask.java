3cf475cbc85304e7cc6ae693151eb550fe6d3fee
jlsuarez <jlsuarez@raulcoka.com>
2020-07-31 13:18:37 -0500

    inicio ruta

--- app/src/main/java/com/caircb/rcbtracegadere/tasks/UserConsultarInicioRutaTask.java
+++ app/src/main/java/com/caircb/rcbtracegadere/tasks/UserConsultarInicioRutaTask.java
@@ -59,8 +59,8 @@ public class UserConsultarInicioRutaTask extends MyRetrofitApi implements Retrof
                                     1);
 
                             MyApp.getDBO().parametroDao().saveOrUpdate("current_ruta",""+response.body().getIdSubRuta());
-                            //MySession.setIdSubruta(response.body().getIdSubRuta());
-                            //MyApp.getDBO().ruteoRecoleccion().saverOrUpdate(new DtoRuteoRecoleccion(response.body().getIdSubRuta(), fechaInicio, 0, null, null, false));
+                            MySession.setIdSubruta(response.body().getIdSubRuta());
+                            MyApp.getDBO().ruteoRecoleccion().saverOrUpdate(new DtoRuteoRecoleccion(response.body().getIdSubRuta(), fechaInicio, 0, null, null, false));
                             message("Ha iniciado previamente sesion, SINCRONICE manifiestos pendientes de recolección");
                             if(mOnRegisterListener!=null)mOnRegisterListener.onSuccessful();
                         }
